eba648ff270fdcbfa9d03b4be0579a1ceefd0580
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-06-25 19:44:07 -0400

    Updates

---
+++ precache-manifest.e9820ddaa3b61ee4693890b2c0efdede.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162798bb6eb8c93280e5cf3749b1c59d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f91ab0d693e9afec71",
+    "url": "/static/css/main.a8c89466.chunk.css"
+  },
+  {
+    "revision": "8bf40c5022259d002bc2",
+    "url": "/static/js/2.3f3b8b5a.chunk.js"
+  },
+  {
+    "revision": "78f91ab0d693e9afec71",
+    "url": "/static/js/main.21667767.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87badba9e8d34b4efd11a1673ae2461b",
+    "url": "/static/media/Background.87badba9.jpg"
+  },
+  {
+    "revision": "8c62d6a22e8220094c4bb3692cdedc35",
+    "url": "/static/media/CleanCodeIcon.8c62d6a2.png"
+  },
+  {
+    "revision": "df2215ab8c9a26721be341d4bbb199c4",
+    "url": "/static/media/CssLogo.df2215ab.png"
+  },
+  {
+    "revision": "058f1fc17d5d5919009922c5ac4357ee",
+    "url": "/static/media/Interactive_icon.058f1fc1.png"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/static/media/JSLogo.0e4fdce8.png"
+  },
+  {
+    "revision": "a09153d7c89463ce619dc6ad65194872",
+    "url": "/static/media/JestLogo.a09153d7.png"
+  },
+  {
+    "revision": "1c5e07f33087d34b77ba56e47bfe8696",
+    "url": "/static/media/NodeLogo.1c5e07f3.jpg"
+  },
+  {
+    "revision": "c5acef4f40648bd12da8ae71fba68184",
+    "url": "/static/media/QuezGames.c5acef4f.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/React.jsLogo.47ce6e77.png"
+  },
+  {
+    "revision": "62cf6a17da4f7ea588ff1cc7b56ebf0f",
+    "url": "/static/media/SchoolManager.62cf6a17.png"
+  },
+  {
+    "revision": "fcf40d6a94df7a5a340cc3d9e9f1aca1",
+    "url": "/static/media/ScssLogo.fcf40d6a.png"
+  },
+  {
+    "revision": "12869654af45bb154deccd39ec67e5d9",
+    "url": "/static/media/Top.12869654.svg"
+  },
+  {
+    "revision": "eae3fca3acccc81b693b0c7d03fd9a98",
+    "url": "/static/media/WilliamsPizza.eae3fca3.png"
+  }
+]);
